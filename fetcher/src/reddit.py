38c78877ccbf25b3056442349f45b783e9e3d034
bahadirgezer <bahadirgezerbg@gmail.com>
2024-02-27 21:59:19 +0300

    comment fetching done

--- fetcher/src/reddit.py
+++ fetcher/src/reddit.py
@@ -1,13 +1,24 @@
 import praw
 import os
+from dotenv import load_dotenv
+
+def get_comments(subreddit, limit):
+    comments = []
+    for submission in reddit.subreddit(subreddit).hot(limit=limit):
+        submission.comments.replace_more(limit=10)
+        for comment in submission.comments.list():
+            comments.append(comment.body)
+
+    return comments
 
 
-reddit = praw.Reddit(
-    client_id="CLIENT_ID",
-    client_secret="CLIENT_SECRET",
-    password="PASSWORD",
-    user_agent="Comment Extraction (by u/USERNAME)",
-    username="USERNAME",
-)
-
-
+if __name__ == "__main__":
+    load_dotenv('../.env')
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+        password=os.getenv("PASSWORD"),
+        user_agent="Comment Extraction (by u/" + os.getenv("USERNAME") + ")",
+        username=os.getenv("USERNAME"),
+    )
+    print(get_comments("all", 10))
