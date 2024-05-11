7d0c8d29388ece9be0112b4d895d8bbe682eef0a
bahadirgezer <bahadirgezerbg@gmail.com>
2024-05-11 12:06:15 +0300

    cleanup processor

--- fetcher/src/processor.py
+++ fetcher/src/processor.py
@@ -9,6 +9,7 @@ from fetcher.src.reddit import RedditPost
 
 
 class TextProcessor:
+
     def __init__(self):
         self._nltk = nltk
         self._nltk.download('punkt', quiet=True)
@@ -24,17 +25,10 @@ class TextProcessor:
         self._link_pattern = r'http\S+'
         self._markdown_link_pattern = r'\[([^\]]+)\]\((http\S+)\)'
 
-    def tokenize(self, txt) -> list[str]:
-        return self._nltk.word_tokenize(txt)
-
-    def sent_tokenize(self, txt) -> list[str]:
-        return self._nltk.sent_tokenize(txt)
-
     def clean_text(self, txt: str) -> str:
         txt = self.lowercase(txt)
         txt = self.replace_links(txt)
         txt = self.remove_punctuation(txt)
-        # txt = self.replace_stopwords(txt) ### we are not moving forward with stopword removal
         return txt
 
     @staticmethod
@@ -53,7 +47,6 @@ class TextProcessor:
 
     @staticmethod
     def remove_punctuation(txt: str) -> str:
-        # remove punctuation except for hyphens and apostrophes, commas, and periods
         return re.sub(r'[^\w\s\-\'\.,]', '', txt)
 
     def replace_links(self, txt: str) -> str:
@@ -79,7 +72,12 @@ class TextProcessor:
 
     def get_sentiment(self, txt: str) -> (float, float, float, float):
         sia = self._nltk.sentiment.SentimentIntensityAnalyzer()
-        return sia.polarity_scores(txt)["compound"], sia.polarity_scores(txt)["pos"], sia.polarity_scores(txt)["neg"], sia.polarity_scores(txt)["neu"]
+        return (
+                sia.polarity_scores(txt)["compound"],
+                sia.polarity_scores(txt)["pos"],
+                sia.polarity_scores(txt)["neg"],
+                sia.polarity_scores(txt)["neu"]
+                )
 
     def clean_posts(self, posts: list[RedditPost]) -> list[RedditPost]:
         for post in posts:
@@ -91,66 +89,5 @@ class TextProcessor:
     def nlp(self):
         return self._nlp
 
-    def construct_dependency_graph(self, text: str):
-        doc = self._nlp(text)
-        # add begin and end attributes to each token
-        for token in doc:
-            print("token: " + token.__str__())
-            print("repr: " + token.__repr__())
-            print("idx: " + token.idx.__repr__())
-            print("text: " + token.text)
-            print("dep: " + token.dep_)
-            print("head: " + token.head.text)
-            print("children: " + token.children.__str__())
-
-
 if __name__ == "__main__":
-
-    processor = TextProcessor()
-    text = (
-        "South Carolina Gov, Henry McMaster held a ceremony Tuesday to spotlight a new law allowing any adult who can "
-        "legally own a gun to carry the weapon openly without a permit."
-    )
-
-    tokens = processor.tokenize(text)
-    lemmatized = processor.lemmatize(tokens)
-    print(lemmatized)
-    pos_tagged = processor.pos_tag(lemmatized)
-    print(pos_tagged)
-
-    doc = processor.nlp(" ".join(tokens))
-
-    entity = "carolina"
-    entity_span = None
-
-    for ent in doc.ents:
-        if entity in ent.text:
-            entity_span = ent
-            break
-
-    # Function to extract sentiment-related words connected to the entity
-    def extract_sentiment_phrases(entity_span: Span):
-        relevant_tokens = []
-        for token in entity_span.root.subtree:
-            if token.dep_ in ["amod", "acomp", "advmod", "neg"]:  # adjectives, adverbs, negations
-                relevant_tokens.append(token)
-        return relevant_tokens
-
-    if entity_span:
-        sentiment_tokens = extract_sentiment_phrases(entity_span)
-        sentiment_text = " ".join([token.text for token in sentiment_tokens])
-        print(f"Sentiment-related text for '{entity}': {sentiment_text}")
-
-        # Analyze sentiment of the relevant text
-        sentiment_doc = processor.nlp(sentiment_text)
-        sentiment = processor.sentiment_analysis(sentiment_text)
-        print(f"Sentiment for '{entity}': {sentiment}")
-    else:
-        print(f"Entity '{entity}' not found.")
-
-    for token in doc:
-        print("=========")
-        print(token.text, "->", token.dep_, "->", token.head.text)
-
-    displacy.serve(doc, style="dep", port=6969)
-    print("done")
+    pass
