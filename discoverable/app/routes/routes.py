8390c5c84f44439c4a926b29e6b1c0702b35abfc
bahadirgezer <bahadirgezerbg@gmail.com>
2024-05-13 16:54:10 +0300

    add occurrence histogram endpoints

--- discoverable/app/routes/routes.py
+++ discoverable/app/routes/routes.py
@@ -19,3 +19,47 @@ def init_routes(app):
             return jsonify(get_wikidata_info(query)), 200
 
         return jsonify(get_basic_info(query)), 200
+
+    @app.route('/histogram/occurrence', methods=['GET'])
+    def occurrence_histogram():
+        wiki_id = request.args.get('id')
+
+        if wiki_id is None or not wiki_id.isdigit():
+            return jsonify({'error': 'Invalid ID'}), 400
+
+        wiki_id = int(wiki_id)
+
+        # Fetch data from database
+        if wiki_id == 4848272:
+            return jsonify(
+                [
+                    {
+                        "wiki_id": 4848272,
+                        "name": "Donald Trump",
+                        "occurrence_count": 39
+                    },
+                    {
+                        "wiki_id": 145422,
+                        "name": "Joe Biden",
+                        "occurrence_count": 2
+                    },
+                    {
+                        "wiki_id": 147301,
+                        "name": "Nancy Pelosi",
+                        "occurrence_count": 1
+                    },
+                    {
+                        "wiki_id": 703464,
+                        "name": "Rob Portman",
+                        "occurrence_count": 1
+                    }
+                ]
+            ), 200
+
+        return jsonify([]), 200
+
+    @app.route('/histogram/sentiment', methods=['GET'])
+    def sentiment_histogram():
+        pass
+
+
